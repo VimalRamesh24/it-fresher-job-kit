7c3dcd46a372ec515914719d43c05818d11694e2
Vimal Ramesh <vimalsaritha2004@gmail.com>
2026-09-07 17:06:14 +0530

    Set test payment to 1 rupee

--- server/server.js
+++ server/server.js
@@ -54,7 +54,7 @@ app.post("/api/create-order", async (req, res) => {
     console.log("Creating Razorpay order...");
 
     const order = await razorpay.orders.create({
-      amount: 9900,
+      amount: 100,
       currency: "INR",
       receipt: `fresher_kit_${Date.now()}`,
     });
